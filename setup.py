49e70599c4f23b75cd64254464f79f298d05cf80
Hajime Sasaki <109297897+jme-rs@users.noreply.github.com>
2024-05-20 09:12:29 +0900

    setup.py を更新

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
     name="pymec",
-    version="0.0.1",
+    version="0.2.2",
+    auther="jme-rs",
     description="Python client for MECRM API",
-    author="jme-rs",
     install_requires=["httpx", "attrs>=23.2.0", "result", "pydantic", "aiofiles"],
+    tests_require=["pytest", "pytest-asyncio"],
     packages=["pymec"],
 )
